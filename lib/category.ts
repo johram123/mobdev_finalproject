fa2f750c0edc89d212853a06be68fef48d82ac96
ShawnFrancia <159972209+ShawnFrancia@users.noreply.github.com>
2025-04-02 19:09:14 -0600

    create category working

---
+++ lib/category.ts
@@ -0,0 +1,53 @@
+import supabase from "./supabase";
+
+const TABLE_NAME = "category";
+
+export async function getCategory()
+{
+    const { data, error } = await supabase.from(TABLE_NAME).select("*");
+    if (error) 
+    {
+        throw error;
+    }
+    return data;
+}
+
+export async function createCategory(category : {category_Name: string})
+{
+    const { category_Name} = category;
+    const { data, error } = await supabase
+    .from(TABLE_NAME)
+    .insert([{ category_Name: category_Name }]);
+    if (error) 
+    {
+        throw error;
+    }
+}
+
+export async function updateCategory(category : 
+    {
+    category_id: number;
+    category_Name: string;
+    }) {
+    const { category_id, category_Name } = category;
+    const { data, error } = await supabase
+    .from(TABLE_NAME)
+    .update({ category_Name: category_Name })
+    .match({ category_id: category_id });
+    if (error) 
+    {
+        throw error;
+    }
+}
+
+export async function deleteCategory(category_id: number) 
+{
+    const { data, error } = await supabase
+    .from(TABLE_NAME)
+    .delete()
+    .match({ category_id: category_id });
+    if (error) 
+    {
+        throw error;
+    }
+}
